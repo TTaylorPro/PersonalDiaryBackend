2adb9464d0fb82600ecd188a717843e3dfa186d2
Travis <travistaylor.email@gmail.com>
2022-08-29 11:24:11 -0400

    Starting project

---
+++ src/main/java/com/revature/PersonalDiaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDiaryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDiaryBackendApplication.class, args);
+	}
+
+}
